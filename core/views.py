973f8f017e9b2abf680290222f4d7328697c8047
Nguyen Son <sonnguyen@tamdongtam.vn>
2019-02-14 23:47:33 +0700

    Thêm category cho navbar

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from product import models as product_models
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'homepage/index.html')
+        cate = product_models.Category.objects.all()
+        return render(request, 'homepage/index.html', {'categorys': cate})
