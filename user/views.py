be8646d0071d7531e6979a249a5150c4ac3ba03a
Nguyen Son <sonnguyen@tamdongtam.vn>
2019-02-14 07:51:11 +0700

    login function

--- user/views.py
+++ user/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # Create your views here.
+
 
 class LoginView(View):
 
     def get(self, request):
-        pass
+        return render(request, 'homepage/login.html')
+
+    def post(self, request):
+        username = request.POST['uname']
+        password = request.POST['psw']
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return HttpResponse('dang nhap khong thanh cong')
+
+        login(request, user)
+        return HttpResponse('thanh cong')
