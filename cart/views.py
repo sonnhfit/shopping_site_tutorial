7b911c74787e2aec36dbf3656e9ceec8327b8a95
Nguyen Son <sonnguyen@tamdongtam.vn>
2019-02-16 15:47:20 +0700

    fix navbar không hiển thị ở view cart

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from product import models as product_models
+from core.utils import *
 # Create your views here.
 
 
 class CartView(View):
     def get(self, request):
-        cate = product_models.Category.objects.all()
-        return render(request, 'homepage/cart_view.html',{'categorys': cate })
+        global_data = get_dict_home_page(request.session.session_key)
+        return render(request, 'homepage/cart_view.html',{'global_data': global_data})
--- core/utils.py
+++ core/utils.py
@@ -6,4 +6,5 @@ from cart.utils import *
 def get_dict_home_page(ss_key):
     cate = Category.objects.all()
     cart_count = get_count_cart(ss_key)
-    return {'categorys': cate, 'cart_count': cart_count}
+    total_price = get_total_price_cart(ss_key)
+    return {'categorys': cate, 'cart_count': cart_count, 'total_price': total_price}
